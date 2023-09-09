bb2d9e860ac65d000acfb4b63a8706d680016ca6
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-09-09 21:53:44 +0530

    profile update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { profileReducer, userReducer } from "./Reducers/userReducer";
-// export const server = 'https://course-bundler-server-e1dx.onrender.com/api/v1';
-export const server = 'http://localhost:4000/api/v1';
+export const server = 'https://course-bundler-server-e1dx.onrender.com/api/v1';
+// export const server = 'http://localhost:4000/api/v1';
 
 const store = configureStore({
     reducer: {
