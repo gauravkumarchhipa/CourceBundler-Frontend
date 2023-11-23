bfd8079d608bef0d6c832bd7cfb17d63c332cf0d
gaurav.chhipa <gaurav.c@zyapaar.com>
2023-11-23 15:26:03 +0530

    conditionaly api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { profileReducer, userReducer } from './reducers/userReducer';
-export const server = 'https://course-bundler-server-e1dx.onrender.com/api/v1';
-// export const server = 'http://localhost:4000/api/v1';
+export const server =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/api/v1'
+    : 'https://course-bundler-server-e1dx.onrender.com/api/v1';
 
 const store = configureStore({
   reducer: {
