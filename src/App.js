e12434c959cb1c2a35a61f18eecbf6ddf4820944
gaurav.chhipa <gaurav.c@zyapaar.com>
2023-11-23 10:46:31 +0530

    cashinsesitive

--- src/App.js
+++ src/App.js
@@ -26,24 +26,26 @@ import CreateCourse from './components/Admin/CreateCourse/CreateCourse';
 import { useDispatch, useSelector } from 'react-redux';
 import toast, { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
-import { loadUser } from './redux/Actions/user';
+import { loadUser } from './redux/actions/user';
 import { ProtectedRoute } from 'protected-route-react';
 import Loader from './components/Layout/Loader';
 function App() {
-  window.addEventListener("contextmenu", (e) => {
-    e.preventDefault()
+  window.addEventListener('contextmenu', e => {
+    e.preventDefault();
   });
 
-  const { isAuthenticated, user, message, error, loading } = useSelector(state => state.user)
+  const { isAuthenticated, user, message, error, loading } = useSelector(
+    state => state.user
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     if (error) {
       toast.error(error);
-      dispatch({ type: 'clearError' })
+      dispatch({ type: 'clearError' });
     }
     if (message) {
       toast.success(message);
-      dispatch({ type: 'clearMessage' })
+      dispatch({ type: 'clearMessage' });
     }
   }, [dispatch, error, message]);
 
@@ -53,42 +55,149 @@ function App() {
 
   return (
     <Router>
-      {
-        loading ? <Loader /> :
-          <>
-            <Header isAuthenticated={isAuthenticated} user={user} />
-            <Routes>
-              {/* *************** User Routes  ***************************/}
-              < Route path='/' element={<Home />} />
-              <Route path='/login' element={<ProtectedRoute isAuthenticated={!isAuthenticated} redirect={'/profile'}><Login /></ProtectedRoute>} />
-              <Route path='/register' element={<ProtectedRoute isAuthenticated={!isAuthenticated} redirect={'/profile'}><Register /></ProtectedRoute>} />
-              <Route path='/forgetpassword' element={<ForgetPassword />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/request' element={<Request />} />
-              <Route path='/courses' element={<Courses />} />
-              <Route path='/courses/:id' element={<CoursePage />} />
-              <Route path='/subscribe' element={<ProtectedRoute isAuthenticated={isAuthenticated}><Subscribe /></ProtectedRoute>} />
-              <Route path='/paymentsuccess' element={<ProtectedRoute isAuthenticated={isAuthenticated}><PaymentSuccess /></ProtectedRoute>} />
-              <Route path='/paymentfail' element={<ProtectedRoute isAuthenticated={isAuthenticated}><PaymentFail /></ProtectedRoute>} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header isAuthenticated={isAuthenticated} user={user} />
+          <Routes>
+            {/* *************** User Routes  ***************************/}
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/login"
+              element={
+                <ProtectedRoute
+                  isAuthenticated={!isAuthenticated}
+                  redirect={'/profile'}
+                >
+                  <Login />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <ProtectedRoute
+                  isAuthenticated={!isAuthenticated}
+                  redirect={'/profile'}
+                >
+                  <Register />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/forgetpassword" element={<ForgetPassword />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/request" element={<Request />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/courses/:id" element={<CoursePage />} />
+            <Route
+              path="/subscribe"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <Subscribe />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/paymentsuccess"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <PaymentSuccess />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/paymentfail"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <PaymentFail />
+                </ProtectedRoute>
+              }
+            />
 
-              <Route path='/profile' element={<ProtectedRoute isAuthenticated={isAuthenticated}><Profile user={user} /></ProtectedRoute>} />
-              <Route path='/changepassword' element={<ProtectedRoute isAuthenticated={isAuthenticated}><ChangePassword /></ProtectedRoute>} />
-              <Route path='/updateprofile' element={<ProtectedRoute isAuthenticated={isAuthenticated}><UpdateProfile /></ProtectedRoute>} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <Profile user={user} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/changepassword"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <ChangePassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/updateprofile"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <UpdateProfile />
+                </ProtectedRoute>
+              }
+            />
 
-              <Route path='/resetpassword/:token' element={<ResetPassword />} />
-              <Route path='*' element={<NotFound />} />
+            <Route path="/resetpassword/:token" element={<ResetPassword />} />
+            <Route path="*" element={<NotFound />} />
 
-              {/* ****************Admin Routes ************************* */}
-              <Route path='/admin/dashboard' element={<ProtectedRoute adminRoute={true} isAuthenticated={isAuthenticated} isAdmin={user && user.role === "admin"}><Dashboard /></ProtectedRoute>} />
-              <Route path='/admin/createcourse' element={<ProtectedRoute adminRoute={true} isAuthenticated={isAuthenticated} isAdmin={user && user.role === "admin"}><CreateCourse /></ProtectedRoute>} />
-              <Route path='/admin/courses' element={<ProtectedRoute adminRoute={true} isAuthenticated={isAuthenticated} isAdmin={user && user.role === "admin"}><AdminCourses /></ProtectedRoute>} />
-              <Route path='/admin/users' element={<ProtectedRoute adminRoute={true} isAuthenticated={isAuthenticated} isAdmin={user && user.role === "admin"}><Users /></ProtectedRoute>} />
-            </Routes>
-            <Footer />
-            <Toaster />
-          </>
-      }
+            {/* ****************Admin Routes ************************* */}
+            <Route
+              path="/admin/dashboard"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/createcourse"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <CreateCourse />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/courses"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <AdminCourses />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <Users />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Footer />
+          <Toaster />
+        </>
+      )}
     </Router>
   );
 }
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,98 +1,118 @@
-import React from 'react'
-import { ColorModeSwitcher } from "../../../ColorModeSwitcher"
-import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, HStack, VStack, useDisclosure } from '@chakra-ui/react';
+import React from 'react';
+import { ColorModeSwitcher } from '../../../ColorModeSwitcher';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  VStack,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { RiDashboard2Fill, RiLogoutBoxLine, RiMenu5Fill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
-import { LinkButton } from './LinkButton'
+import { LinkButton } from './LinkButton';
 import { useDispatch } from 'react-redux';
-import { logout } from '../../../redux/Actions/user';
+import { logout } from '../../../redux/actions/user';
 
 const Header = ({ isAuthenticated = false, user }) => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const dispatch = useDispatch();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
 
-    // const isAuthenticated = false;
-    // const user = {
-    //     role: 'admin',
-    // }
+  // const isAuthenticated = false;
+  // const user = {
+  //     role: 'admin',
+  // }
 
-    const logoutHandler = () => {
-        onClose()
-        dispatch(logout());
-    }
-    return (
-        <>
-            <ColorModeSwitcher />
-            <Button
-                onClick={onOpen}
-                colorScheme={'yellow'}
-                width={'12'}
-                height={'12'}
-                rounded={'full'}
-                zIndex={'overlay'}
-                position={"fixed"}
-                top={6}
-                left={6}
-            >
-                <RiMenu5Fill />
-            </Button>
-            <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay backdropFilter={'blur(3px'} />
-                <DrawerContent >
-                    <DrawerHeader borderBottomWidth={'1px'}>COURSE BUNDLER</DrawerHeader>
-                    <DrawerBody>
-                        <VStack spacing={"4"} alignItems={"flex-start"}>
-                            <LinkButton onClose={onClose} url="/" title="Home" />
-                            <LinkButton onClose={onClose} url="/courses" title="Browse All Courses" />
-                            <LinkButton onClose={onClose} url="/request" title="Request a Course" />
-                            <LinkButton onClose={onClose} url="/contact" title="Contact Us" />
-                            <LinkButton onClose={onClose} url="/about" title="About" />
-                            <HStack
-                                justifyContent={"space-evenly"}
-                                position={"absolute"}
-                                bottom={"2rem"}
-                                width={"80%"}
-                            >
-                                {
-                                    isAuthenticated ? (
-                                        <>
-                                            <VStack>
-                                                <HStack>
-                                                    <Link to="/profile" onClick={onClose}>
-                                                        <Button colorScheme='yellow' variant={'ghost'}>Profile</Button>
-                                                    </Link>
-                                                    <Button variant={'ghost'} onClick={logoutHandler}>
-                                                        <RiLogoutBoxLine />
-                                                        Logout
-                                                    </Button>
-                                                </HStack>
-                                                {
-                                                    user && user?.role === "admin" && <Link to="/admin/dashboard" onClick={onClose}>
-                                                        <Button colorScheme={'purple'} variant={"ghost"}>
-                                                            <RiDashboard2Fill style={{ margin: "4px" }} />
-                                                            Dashboard
-                                                        </Button>
-                                                    </Link>
-                                                }
-                                            </VStack>
-                                        </>
-                                    ) : <>
-                                        <Link to="/login" onClick={onClose}>
-                                            <Button colorScheme='yellow'>Login</Button>
-                                        </Link>
-                                        <p>OR</p>
-                                        <Link to="/register" onClick={onClose}>
-                                            <Button colorScheme='yellow'>SIgn Up</Button>
-                                        </Link>
-                                    </>
-                                }
-                            </HStack>
-                        </VStack>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
-        </>
-    )
-}
+  const logoutHandler = () => {
+    onClose();
+    dispatch(logout());
+  };
+  return (
+    <>
+      <ColorModeSwitcher />
+      <Button
+        onClick={onOpen}
+        colorScheme={'yellow'}
+        width={'12'}
+        height={'12'}
+        rounded={'full'}
+        zIndex={'overlay'}
+        position={'fixed'}
+        top={6}
+        left={6}
+      >
+        <RiMenu5Fill />
+      </Button>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay backdropFilter={'blur(3px'} />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth={'1px'}>COURSE BUNDLER</DrawerHeader>
+          <DrawerBody>
+            <VStack spacing={'4'} alignItems={'flex-start'}>
+              <LinkButton onClose={onClose} url="/" title="Home" />
+              <LinkButton
+                onClose={onClose}
+                url="/courses"
+                title="Browse All Courses"
+              />
+              <LinkButton
+                onClose={onClose}
+                url="/request"
+                title="Request a Course"
+              />
+              <LinkButton onClose={onClose} url="/contact" title="Contact Us" />
+              <LinkButton onClose={onClose} url="/about" title="About" />
+              <HStack
+                justifyContent={'space-evenly'}
+                position={'absolute'}
+                bottom={'2rem'}
+                width={'80%'}
+              >
+                {isAuthenticated ? (
+                  <>
+                    <VStack>
+                      <HStack>
+                        <Link to="/profile" onClick={onClose}>
+                          <Button colorScheme="yellow" variant={'ghost'}>
+                            Profile
+                          </Button>
+                        </Link>
+                        <Button variant={'ghost'} onClick={logoutHandler}>
+                          <RiLogoutBoxLine />
+                          Logout
+                        </Button>
+                      </HStack>
+                      {user && user?.role === 'admin' && (
+                        <Link to="/admin/dashboard" onClick={onClose}>
+                          <Button colorScheme={'purple'} variant={'ghost'}>
+                            <RiDashboard2Fill style={{ margin: '4px' }} />
+                            Dashboard
+                          </Button>
+                        </Link>
+                      )}
+                    </VStack>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/login" onClick={onClose}>
+                      <Button colorScheme="yellow">Login</Button>
+                    </Link>
+                    <p>OR</p>
+                    <Link to="/register" onClick={onClose}>
+                      <Button colorScheme="yellow">SIgn Up</Button>
+                    </Link>
+                  </>
+                )}
+              </HStack>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
 
 export default Header;
